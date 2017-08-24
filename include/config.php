4dc8b6f29667696ddb668e7afa7f0c23cd149391
Blake Gilmore <bgi25@uclive.ac.nz>
2017-08-25 11:43:53 +1200

    Following the tutorial: Got an api key for the auckland transport api, this key is in config.php Got an api key for google maps and inserted it into index.php, also some code put into various files to make it work as described in ('tutorial_steps_for_google_maps.txt')

--- include/config.php
+++ include/config.php
@@ -3,5 +3,5 @@ $hostname = "csse-info263.canterbury.ac.nz";
 $database = "akl_transport";
 $username = "info263";
 $password = "info263";
-$APIKey = ""; # Your API Key here.
+$APIKey = "f8a4540490ba4748bb37cf1e6d3c4b3e"; # Your API Key here.
 ?>
--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 $active = "home";
 require_once 'include/header.php';
 ?>
-
+<div id="map"></div>
 <script async defer
-  src="https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_API_KEY_HERE&callback=initMap">
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDr0a376m4pWJKroaZ5qXCLTq2-pHPoxOY&callback=initMap">
 </script>
 <script src="scripts/map.js"></script>
 <script>
